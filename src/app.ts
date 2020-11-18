6d9b115bae3a7fc67dfa7ade9e7de1293d642b0b
asanchez <ing.albert.sanchez91@gmail.com>
2020-11-18 17:43:57 -0300

    [feature] initial structure

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hola mundo Nodejs + TS');
+});
+
+app.listen(port, ( ) => {
+    console.log(`server is listening on ${port}`);
+});
